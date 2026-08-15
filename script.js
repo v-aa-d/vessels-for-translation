f42fad21b111266ad1b32afa732e9cd1e469c27b
v-aa-d <veroarancibiadaes@gmail.com>
2026-08-15 14:06:38 +0200

    added text

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function typeTitle() {
 typeTitle();
 
 const cursorImg = document.getElementById("cursor-img");
+const cursorText = document.getElementById("cursor-text");
 const links = document.querySelectorAll(".links a[data-img]");
 
 const offset = 20;
@@ -22,6 +23,17 @@ const offset = 20;
 document.addEventListener("mousemove", (e) => {
   cursorImg.style.left = e.clientX + offset + "px";
   cursorImg.style.top = e.clientY + offset + "px";
+  cursorText.style.left = e.clientX + offset + "px";
+  cursorText.style.top = e.clientY + offset + "px";
+});
+
+title.addEventListener("mouseenter", () => {
+  cursorText.textContent = title.dataset.intro;
+  cursorText.style.display = "block";
+});
+
+title.addEventListener("mouseleave", () => {
+  cursorText.style.display = "none";
 });
 
 links.forEach((link) => {
